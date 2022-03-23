a753ca623f27df9d9a9e50bf08e46c2e328b1f48
CoolSnow <670696652@qq.com>
2022-03-23 21:23:04 +0800

    feat: add example

---
+++ example/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import AudioPlayer from 'vue3-audio-player'
+</script>
+
+<template>
+  <div>
+    <AudioPlayer />
+  </div>
+</template>
+
+<style>
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
